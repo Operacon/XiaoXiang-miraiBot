5f587234434d9a0d4173f3cbed6beec9c6558789
Operacon <anlan18437@hotmail.com>
2022-03-03 14:33:08 +0800

    反转复读和 chatbot 扫描顺序；加入介绍回复

--- src/main/kotlin/bean/GlobalVars.kt
+++ src/main/kotlin/bean/GlobalVars.kt
@@ -10,5 +10,5 @@ package org.operacon.bean
 object GlobalVars {
     val splitter = Regex(""" +""")
     val atReplace = Regex("""@[0-9]{5,10} ?""")
-    val pagReplace = Regex("\\[.+?\\]")
+    val pagReplace = Regex("""\\[.+?\\]""")
 }
