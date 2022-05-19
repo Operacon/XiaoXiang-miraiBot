1053910eb11622241a837fbd4cde7ee71286659d
FuryMartin <41051953+FuryMartin@users.noreply.github.com>
2022-05-20 03:54:28 +0800

    词云改善 (#1)
    * 添加词云蒙版，改善词云观感
    
    * 添加词云蒙版图片
    
    * 更新停用词表
    
    * 使用 skimage 来处理蒙版图片
    
    Co-authored-by: Operacon <anlan18437@hotmail.com>

--- WordCloud/Service.py
+++ WordCloud/Service.py
@@ -5,11 +5,21 @@
 import io
 import pkuseg
 import wordcloud
-
+import skimage
+import re
 
 # 使用 pkuseg 进行分词，由于一天一个群的聊天记录规模不算太大，单进程可以接受
 #       此处可以在同目录下的 reserved.txt 定制不会被拆分的词组
 seg = pkuseg.pkuseg(user_dict="./reserved.txt")
+
+# 生成词云的蒙版图片，前景和背景对比应当比较强烈
+#       替换同目录下的 mask.png 来定制蒙版
+enable_mask = True
+mask = skimage.io.imread("./mask.png")
+mask = skimage.img_as_ubyte(
+    skimage.transform.resize(
+        mask, (1344 / mask.shape[1] * mask.shape[0], 1344)))
+colors = wordcloud.ImageColorGenerator(mask)
 
 # 配置不在词云中出现的 stopwords
 sw = []
@@ -21,13 +31,28 @@ with open("stopwords.txt", "r", encoding="UTF-8") as swFile:
 # 配置传入词云前多少个最常出现的词（用于改善性能）
 maxT = 25
 
-# 使用 wordcloud 进行绘制，注意传入的参数，可自己定制外观
+# wordcloud 进行绘制使用的字体和背景颜色
 #       此处必须指定系统安装的含有简中的字体，或者放进本目录
-wc = wordcloud.WordCloud(width=1344, height=2772, background_color='white',
-                         font_path="simkai.ttf")
+#       颜色格式为标准色名或 rgb 函数，前景色默认为蒙版前景色，如不启用蒙版则随机生成
+font_path = "simkai.ttf"
+background_color = "rgb(250, 235, 215)"  # "black"
+
+# 可重写 color_func 来修改前景色
+if enable_mask:
+    wc = wordcloud.WordCloud(background_color=background_color,
+                             mask=mask, color_func=colors,
+                             font_path=font_path)
+else:
+    wc = wordcloud.WordCloud(width=1344, height=2772,
+                             background_color=background_color,
+                             font_path=font_path)
+urlRe = re.compile(r"[a-zA-z]+://[^\s]*")
 
 
 def geneImg(t):
+    # 去除url链接
+    t = re.sub(urlRe, "", t)
+
     lis = seg.cut(t)
     maxN = min(maxT, lis.__len__())
     dic = {}
